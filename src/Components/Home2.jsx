eb8c56d39dd4b9b870c8993433b81558d518e6d6
Karunesh Ranjan <karunesh029@gmail.com>
2022-05-03 19:51:02 +0530

    Update Home2.jsx

--- src/Components/Home2.jsx
+++ src/Components/Home2.jsx
@@ -38,73 +38,73 @@ const Home2 = () => {
         )
     }
      
-//     return( 
+    return( 
        
-//     <div className="homemid"> 
-//         <h2>All Lesson</h2>    
+    <div className="homemid"> 
+        <h2>All Lesson</h2>    
          
-//         <div className="home2">{data.map((item) => {
-//             return (<div className="home21">
-//                <Link to="/Topic"> <img  src = {item.profile_img} alt = "" /> </Link>
-//                     <div>
-//                          <h2>{item.title}</h2> 
-//                     </div>
-//                     <div>
-//                         <h5>{item.name}</h5> 
-//                     </div>
-//                     <div>
-//                         <p>{item.role}</p>
-//                     </div>
-//             </div>)
-//         })}</div>
-
-//     </div>)
-    
-    
-    
-       return( 
-        <div className="outer">
-             <h1>All Lesson</h1>        
-          
-                <div className="outer-container">{data.map((item) => {
-                   
-                    return (<div className="inner-container">
-                    <div className="img-div">
-                    <Link to="/Topic">     <img className="main-img" src = {item.profile_img} alt = ""/> </Link>
+        <div className="home2">{data.map((item) => {
+            return (<div className="home21">
+               <Link to="/Topic"> <img  src = {item.profile_img} alt = "" /> </Link>
+                    <div>
+                         <h2>{item.title}</h2> 
                     </div>
+                    <div>
+                        <h5>{item.name}</h5> 
+                    </div>
+                    <div>
+                        <p>{item.role}</p>
+                    </div>
+            </div>)
+        })}</div>
+
+    </div>)
     
-                    <div class="content-div">
-                  <div class="upper-title">
-                     <p class="designation">
-                     {item.title}
-                     </p>
-                  </div>
-                  <div class="bottom-div">
-                      <div class="left-div">
-                      <img className="sub-img"  src = {item.profile_img} alt = "" />
-                       </div>  
     
-                       <div class="name-div">
-                        <h5 class="dark">
-                        {item.name}
-                        </h5>
-                        <h6 class="light">
-                        {item.role}
-                        </h6>
-                       </div>
-                       <div class="right-div">
-                        <h4 class="number">{item.total_count}</h4>
-                        <p class="vid">VIDEOS</p>
-                       </div>
     
-                       </div>
-                       </div>
-                       </div>)
+//        return( 
+//         <div className="outer">
+//              <h1>All Lesson</h1>        
+          
+//                 <div className="outer-container">{data.map((item) => {
                    
-                })}</div>
+//                     return (<div className="inner-container">
+//                     <div className="img-div">
+//                     <Link to="/Topic">     <img className="main-img" src = {item.profile_img} alt = ""/> </Link>
+//                     </div>
+    
+//                     <div class="content-div">
+//                   <div class="upper-title">
+//                      <p class="designation">
+//                      {item.title}
+//                      </p>
+//                   </div>
+//                   <div class="bottom-div">
+//                       <div class="left-div">
+//                       <img className="sub-img"  src = {item.profile_img} alt = "" />
+//                        </div>  
+    
+//                        <div class="name-div">
+//                         <h5 class="dark">
+//                         {item.name}
+//                         </h5>
+//                         <h6 class="light">
+//                         {item.role}
+//                         </h6>
+//                        </div>
+//                        <div class="right-div">
+//                         <h4 class="number">{item.total_count}</h4>
+//                         <p class="vid">VIDEOS</p>
+//                        </div>
+    
+//                        </div>
+//                        </div>
+//                        </div>)
+                   
+//                 })}</div>
           
     
-        </div>)
+//         </div>)
 }
 
 export { Home2 }
